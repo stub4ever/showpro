d6953eda1dc0a5684d37e62319709adff9fbadd3
Toby Bui <tobybuicom@gmail.com>
2021-01-18 07:35:31 +0700

    Dynamic routing

---
+++ pages/[country]/[showId].js
@@ -0,0 +1,4 @@
+const ShowDetails = () => <h1>Show Details</h1>;
+
+// /[country]/[id]  => /US/32332 , /EN/32344
+export default ShowDetails;
---
+++ pages/[country]/index.js
@@ -0,0 +1,4 @@
+const CountryIndex = () => <h1>This is country index page</h1>;
+
+// /[country]/  => /US/ , /EN/
+export default CountryIndex;
