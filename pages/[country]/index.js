f031652e5be313fcecb193f8f31b747c29f3fc30
Toby Bui <tobybuicom@gmail.com>
2021-01-19 07:31:07 +0700

    fetching data on server side

--- pages/[country]/index.js
+++ pages/[country]/index.js
@@ -1,18 +1,28 @@
 import { useEffect } from "react";
 import axios from "axios";
 
-const CountryIndex = () => {
-  useEffect(() => {
-    axios
-      .get("http://api.tvmaze.com/schedule?country=US&date=2014-12-01") // fetch data via the browser(Client) with axios not service side
-      .then((response) => console.log(response.data));
-  }, []); // Only when the component is mounting for the first time
+const CountryIndex = (props) => {
+  console.log(`TCL: Home -> props ${props.shows}`);
 
   return (
     <>
       <h1>This is country index page</h1>
     </>
   );
+};
+
+// Initial as props
+CountryIndex.getInitialProps = async () => {
+  // fetch data via the server-side with axios not client side
+  const response = await axios.get(
+    "http://api.tvmaze.com/schedule?country=US&date=2014-12-01"
+  );
+  console.log(`TCL: Home -> response ${response.data}`); // return log inside terminal
+
+  return {
+    shows: response.data,
+    test: "Testing",
+  };
 };
 
 // /[country]/  => /US/ , /EN/
