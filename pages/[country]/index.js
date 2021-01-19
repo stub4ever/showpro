cc0043472e3812063784f2fff7794d28870cee25
Toby Bui <tobybuicom@gmail.com>
2021-01-19 07:18:40 +0700

    fetching data on client side

--- pages/[country]/index.js
+++ pages/[country]/index.js
@@ -1,4 +1,19 @@
-const CountryIndex = () => <h1>This is country index page</h1>;
+import { useEffect } from "react";
+import axios from "axios";
+
+const CountryIndex = () => {
+  useEffect(() => {
+    axios
+      .get("http://api.tvmaze.com/schedule?country=US&date=2014-12-01") // fetch data via the browser(Client) with axios not service side
+      .then((response) => console.log(response.data));
+  }, []); // Only when the component is mounting for the first time
+
+  return (
+    <>
+      <h1>This is country index page</h1>
+    </>
+  );
+};
 
 // /[country]/  => /US/ , /EN/
 export default CountryIndex;
