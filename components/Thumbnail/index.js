41227b55a61dde1a0e98ee1be0117f1000c4f97b
Toby Bui <tobybuicom@gmail.com>
2021-01-21 07:50:07 +0700

    extracting styles to a separate file

--- components/Thumbnail/index.js
+++ components/Thumbnail/index.js
@@ -1,9 +1,13 @@
+import ThumbnailStyles from "./styles";
+
 const Thumbnail = ({ imageUrl, caption }) => {
   return (
     <>
       <div className="thumbnail">
         <img className="thumbnail__image" src={imageUrl}></img>
-        <h3 className="thumbnail__title">{caption}</h3>
+        <h3 className="thumbnail__caption">{caption}</h3>
+
+        <style jsx>{ThumbnailStyles}</style>
       </div>
     </>
   );
