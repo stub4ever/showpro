4424c33433ac72062d09a086b777877819c85080
Toby Bui <tobybuicom@gmail.com>
2021-01-22 07:29:21 +0700

    Linkcomponent navigating on client-side

--- components/Thumbnail/index.js
+++ components/Thumbnail/index.js
@@ -8,9 +8,9 @@ const Thumbnail = ({
   return (
     <>
       <div className="thumbnail">
-        {/* href="/[country]/[id]" */}
+        {/* href="/[country]/[id]" as="/us/5617" */}
         {/* Currently the linked page reload completely, you can update the client-side without complete fetch on the server-side */}
-        <Link href="/us/5617">
+        <Link href="/[country]/[showId]" as="/us/5617">
           <a>
             <img className="thumbnail__image" src={imageUrl}></img>
             <h3 className="thumbnail__caption">{caption}</h3>
