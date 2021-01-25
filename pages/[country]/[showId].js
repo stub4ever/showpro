b17796ba7724cbb156041b190d2ca73532139c63
Toby Bui <tobybuicom@gmail.com>
2021-01-25 07:22:40 +0700

    Add details Page review and requirements

--- pages/[country]/[showId].js
+++ pages/[country]/[showId].js
@@ -1,4 +1,12 @@
-const ShowDetails = () => <h1>Show Details</h1>;
+const ShowDetails = ({ posterUrl, title }) => {
+  return (
+    <div className="show-details">
+      <div className="show-details__poster">
+        <h1>Any title</h1>
+      </div>
+    </div>
+  );
+};
 
 // /[country]/[id]  => /US/32332 , /EN/32344
 export default ShowDetails;
