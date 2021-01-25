8e0c0ac8b97a12bd0f30933f79db1021c7118589
Toby Bui <tobybuicom@gmail.com>
2021-01-25 07:42:29 +0700

    creating poser and title

--- pages/[country]/[showId].js
+++ pages/[country]/[showId].js
@@ -1,11 +1,32 @@
-const ShowDetails = ({ posterUrl, title }) => {
+import axios from "axios";
+
+const ShowDetails = ({ show }) => {
+  const { name, image } = show;
+
   return (
     <div className="show-details">
-      <div className="show-details__poster">
-        <h1>Any title</h1>
-      </div>
+      <div
+        className="show-details__poster"
+        style={{ backgroundImage: `url(${image && image.original})` }}
+      ></div>
+      <h1>{name}</h1>
+
+      <style jsx>{`
+        .show-details__poster {
+          height: 200px;
+          background-size: cover;
+        }
+      `}</style>
     </div>
   );
+};
+
+ShowDetails.getInitialProps = async () => {
+  const response = await axios.get(`https://api.tvmaze.com/shows/1?embed=cast`);
+
+  return {
+    show: response.data,
+  };
 };
 
 // /[country]/[id]  => /US/32332 , /EN/32344
